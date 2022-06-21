0c5792030dd92d58090ca8537f7bf69ff3503dcd
marcograhl <manograhl@gmail.com>
2022-06-21 09:42:27 +0000

    added gitpod yml and vite server hmr config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,12 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind({})],
+    vite: {
+        server: {
+            hmr: {
+                clientPort: process.env.HMR_HOST ? 443 : 24678,
+                host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+            }
+        }
+    }
 });
